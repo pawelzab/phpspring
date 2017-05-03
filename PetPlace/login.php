0ebc8ddf596908dbbb6aabf85efb97a64016236e
Charlotte Sears <csears@mac.hush.com>
2017-05-03 18:20:09 -0400

    new Login and doLogin files

--- PetPlace/login.php
+++ PetPlace/login.php
@@ -1,58 +1,49 @@
 <?php
-    session_start();
-    error_reporting( E_ERROR | E_WARNING | E_PARSE);
+	error_reporting(E_ERROR | E_WARNING | E_PARSE);
+	session_start(); // starts the session so we can put things in the session superglobal (i.e. an error message)
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-    <head>
-        <title>Sign In</title>
-    </head>
-    <body>
-        <div>
-            <h1>Sign In</h1><hr />
-        </div>
-        <?php if(isset($_SESSION['errMsg'])){ ?>
-            <div class="error-message"><?=$_SESSION['errMsg']?></div>
-        <?php } ?>
-         <?php if(isset($_SESSION['$faillogin'])){ ?>
-            <div class="failed-login"><?=$_SESSION['$faillogin']?></div>
-        <?php } ?>
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+	</head>
+	<body>
+		<div>
+			<h1>Login</h1><hr />
+		</div>
+	<?php if(isset($_SESSION['errMsg'])){ ?> <!---- closes php so can write a block of html ---->
+		<div class="error-message"><?=$_SESSION['errMsg']?></div>
+		
+	<?php } ?>
+				<form name="loginForm" action="doLogin.php" method="post">
+					<table width="80%">
+						<tr>
+							<td><label for="email">Email</label></td>
+							<td><input type="text"
+								       id="email" 
+								       name="email"
+								       value="<?=$_SESSION['email']?>" /></td>
+						</tr>
+						<tr>
+							<td><label for="password">Password</label></td>
+							<td><input type="text" 
+								       id="password" 
+								       name="password"
+								       </td>
+						</tr>
+						<tr>
+							<td colspan="2" align="right">
+								<input type="submit" value="Log In">
+							</td>
+						</tr>
+						<?php // unset the variables because we just finished using them
+							
+							unset($_SESSION['email']);
+							unset($_SESSION['password']);	
+							unset($_SESSION['errMsg']);
 
-
-        <div>
-            <form name="loginForm" action="doLogin.php" method="post">
-                <table width="80%">
-                    <tr>
-                        <td><label for="email">email</label></td>
-                        <td><input type="text"
-                                   id="email"
-                                   name="email"
-                                   value="<?=$_SESSION['email']?>" /></td>
-                    </tr>
-                    <tr>
-                        <td><label for="password">password</label></td>
-                        <td><input type="password"
-                                   id="password"
-                                   name="password"/></td>
-                    </tr>
-                    <tr>
-                        <td><label for="password2"> confirm your password</label></td>
-                        <td><input type="password"
-                                   id="password2"
-                                   name="password2"/></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" align="right"><input type="submit" value="Login" /></td>
-                    </tr>
-
-                    <?php
-                        unset($_SESSION['email']);
-                        
-                    ?>
-
-                </table>
-            </form>
-            <a href="index.php">go back</a>
-        </div>
-    </body>
-</html>
+							
+						?>
+					</table>
+				</form>
